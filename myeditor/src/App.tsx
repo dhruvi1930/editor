081a010de482c2b0687fa2003963755eeeba8a28
Dhruvi <dhruvihl369@gmail.com>
2023-08-17 21:58:28 -0400

    added title and editors with preview

--- myeditor/src/App.tsx
+++ myeditor/src/App.tsx
@@ -1,23 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism.css";
+import "./App.css";
 
 function App() {
+  const [htmlCode, setHtmlCode] = useState("");
+  const [cssCode, setCssCode] = useState("");
+
+  const handleHtmlChange = (newHtmlCode: string) => {
+    setHtmlCode(newHtmlCode);
+
+    if (
+      newHtmlCode.includes("<script>") ||
+      newHtmlCode.includes("<iframe ") ||
+      newHtmlCode.includes("<style>") ||
+      newHtmlCode.includes("<iframe>")
+    ) {
+      window.alert("WARNING: <script> or <iframe> or <style> tags detected!");
+    }
+  };
+
+  const handleCssChange = (newCssCode: string) => {
+    setCssCode(newCssCode);
+    if (
+      newCssCode.includes("<script>") ||
+      newCssCode.includes("<iframe ") ||
+      newCssCode.includes("<style>") ||
+      newCssCode.includes("<iframe>")
+    ) {
+      window.alert("WARNING: <script> or <iframe> or <style> tags detected!");
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="App-heading">Welcome to HTML and CSS Editor</p>
+        <div className="spacer" />
+        <div className="EditorContainer">
+          <div className="editor">
+            <p className="editorTitle">HTML EDITOR</p>
+            <Editor
+              value={htmlCode}
+              onValueChange={handleHtmlChange}
+              highlight={(htmlCode) =>
+                highlight(htmlCode, languages.markup, "markup")
+              }
+              padding={10}
+              style={{
+                height: "250px",
+                overflow: "auto",
+                fontSize: 16,
+                border: "2px solid black",
+                borderRadius: "4px",
+                backgroundColor: "white",
+                color: "black",
+                margin: "5%",
+              }}
+            />
+          </div>
+          <div className="editor">
+            <p className="editorTitle">CSS EDITOR</p>
+            <Editor
+              value={cssCode}
+              onValueChange={handleCssChange}
+              highlight={(code) => highlight(code, languages.css, "css")}
+              padding={10}
+              style={{
+                height: "250px",
+                overflow: "auto",
+                fontSize: 16,
+                border: "2px solid black",
+                borderRadius: "4px",
+                backgroundColor: "white",
+                color: "black",
+                margin: "5%",
+              }}
+            />
+          </div>
+        </div>
+        <iframe
+          title="Preview"
+          className="preview-iframe"
+          srcDoc={`<style>${cssCode}</style>${htmlCode}`}
+        />
       </header>
     </div>
   );
